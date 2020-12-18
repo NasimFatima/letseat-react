9cc028afe9a122830311c01127bcd17280e92826
nasimfatima <fatimawaheed143@gmail.com>
2020-12-18 13:34:40 +0500

    remove app.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { Router } from './routes/routesMapping'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
